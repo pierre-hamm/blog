f8c1e06d427b2c10937c18e79537b28d4132df0c
Pierre Hamm <pi3rre.hamm@gmail.com>
2016-12-14 15:05:13 +0100

    create Article + use article repository

--- src/M2I/BlogBundle/Controller/IndexController.php
+++ src/M2I/BlogBundle/Controller/IndexController.php
@@ -2,13 +2,36 @@
 
 namespace M2I\BlogBundle\Controller;
 
+use M2I\BlogBundle\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class IndexController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('M2IBlogBundle:Index:index.html.twig');
+        $em = $this->container->get('doctrine.orm.entity_manager');
+        $articleRepository = $em->getRepository('M2IBlogBundle:Article');
+
+        // tous les articles
+        $articleList = $articleRepository->findAll();
+
+        // get article id = 2 (un tableau)
+        $article2 = $articleRepository->find(2);
+        $article2 = $articleRepository->findById(2);
+
+        // get article avc title = 'Article 3' (un tableau)
+        $article3 = $articleRepository->findByTitle('Article 3');
+
+        // recupere l'article 3
+        $oneArticle = $articleRepository->findOneById(3);
+
+        dump($article2);
+        dump($article3);
+        die();
+
+        return $this->render(
+            'M2IBlogBundle:Index:index.html.twig'
+        );
     }
 
     public function contactAction()
